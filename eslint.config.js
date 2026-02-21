66ee936b0739511a29f22d77d1de91755dc79944
Lucas Fidelis <lucasm241301@gmail.com>
2026-02-20 21:15:33 -0300

    chore: initial setup

---
+++ eslint.config.js
@@ -0,0 +1,87 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import cypress from "eslint-plugin-cypress";
+import importPlugin from "eslint-plugin-import";
+import unusedImports from "eslint-plugin-unused-imports";
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.ts"],
+    ignores: [
+      "node_modules",
+      "dist",
+      "coverage",
+      "cypress/videos",
+      "cypress/screenshots",
+    ],
+    languageOptions: {
+      parserOptions: {
+        project: ["./tsconfig.json", "./tsconfig.cypress.json"],
+        tsconfigRootDir: import.meta.dirname,
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      cypress,
+      import: importPlugin,
+      "unused-imports": unusedImports,
+    },
+    rules: {
+      /* Cypress */
+      "cypress/no-unnecessary-waiting": "warn",
+      "cypress/assertion-before-screenshot": "warn",
+
+      /* TS */
+      "@typescript-eslint/no-unused-vars": "off",
+      "unused-imports/no-unused-imports": "error",
+      "@typescript-eslint/no-explicit-any": "warn",
+
+      /* Imports */
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+          ],
+          "newlines-between": "always",
+        },
+      ],
+
+      /* Style */
+      quotes: [
+        "error",
+        "single",
+        {
+          avoidEscape: true,
+          allowTemplateLiterals: false,
+        },
+      ],
+
+      "no-console": "off",
+    },
+  },
+
+  {
+    files: ["cypress/**/*.ts"],
+    languageOptions: {
+      globals: {
+        cy: "readonly",
+        Cypress: "readonly",
+        expect: "readonly",
+        describe: "readonly",
+        it: "readonly",
+        before: "readonly",
+        beforeEach: "readonly",
+        after: "readonly",
+        afterEach: "readonly",
+      },
+    },
+  },
+];
