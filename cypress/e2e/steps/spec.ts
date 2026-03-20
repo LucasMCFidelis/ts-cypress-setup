e00b3104cab6251f4b4dfb7330471a339e22f145
Lucas Fidelis <lucasm241301@gmail.com>
2026-03-20 17:20:23 -0300

    style: apply lint and format fixes across codebase

--- cypress/e2e/steps/spec.ts
+++ cypress/e2e/steps/spec.ts
@@ -1,4 +1,4 @@
-import { When, Then, Given } from "@badeball/cypress-cucumber-preprocessor";
+import { When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
 When("I visit duckduckgo.com", () => {
   cy.visit("https://www.duckduckgo.com");
